73b552735811baf8d9ed2e43f810e3f5068ab9bd
Hayden-Neilson <haydenneilson5@gmail.com>
2020-05-15 12:19:13 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,58 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import axios from "axios";
+import Home from "./home";
+import NavigationContainer from "../components/navigation-container/navigation";
+import About from "./about";
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.HandleGetJobs = this.HandleGetJobs.bind(this);
+  }
+
+  HandleGetJobs() {
+    axios
+      .get("https://hjnapicap.herokuapp.com/all_job")
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+
+  componentWillMount() {
+    this.HandleGetJobs;
+  }
+
+  render() {
+    return (
+      <div className="container">
+        <Router>
+          <div>
+            <NavigationContainer />
+
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route path="/About" component={About} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    );
+  }
+}
+
+// import React, { useContext, useEffect } from 'react';
+
+// function App  {
+//   useEffect(() => {
+//     fetch('/')
+//   })
+//     return (
+//       <div className='app'>
+//         <h1>Scraper to find a job</h1>
+//       </div>
+//     );
